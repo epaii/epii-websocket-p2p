33d4ca7ede015f3bdb01d1900014fa6c43d9e26a
epii <rlr@example.com>
2022-06-28 11:35:45 +0800

    rlr

--- client/src/websocket.js
+++ client/src/websocket.js
@@ -1,4 +1,22 @@
 
+function wrapPromise(fun) {
+    let outfun = fun;
+    let outRet = null;
+    if (!fun) {
+        let _ok = null;
+        outfun = function (ret) {
+            if (_ok) {
+                _ok(ret);
+            }
+        }
+        return [outfun, new Promise((ok, error) => {
+            _ok = ok;
+        })]
+    } else {
+        return [outRet, null];
+    }
+}
+
 class epii_websocket {
     constructor(url, epii_id, info = {}) {
         this.ready_callbacks = [];
@@ -7,14 +25,14 @@ class epii_websocket {
         this.epii_info = info;
         this.epii_servers = {};
         this.cbs = [];
-        this.__on_error =null;
+        this.__on_error = null;
         this._start();
         this._close_by_self = false;
-        this.onUserAvailable_cbs ={};
+        this.onUserAvailable_cbs = {};
         this.heart_rate_runing = false;
-        if(info.heart_rate){
+        if (info.heart_rate) {
             this.heart_rate = info.heart_rate;
-        }else{
+        } else {
             this.heart_rate = 30000;
         }
     }
@@ -24,61 +42,62 @@ class epii_websocket {
         this.cbs.push(cb);
         return l;
     }
-    _heart_rate(){
-        if(!this.heart_rate_runing){
+    _heart_rate() {
+        if (!this.heart_rate_runing) {
             this.heart_rate_runing = true;
             setInterval(() => {
-                if(this.is_ready){
-                    
-                    this.ws.send(JSON.stringify({hr:1}))
+                if (this.is_ready) {
+
+                    this.ws.send(JSON.stringify({ hr: 1 }))
                 }
             }, this.heart_rate);
         }
     }
     _start() {
-        
+
         this.is_ready = false;
         this._close_by_self = false;
         if (typeof uni != "undefined") {
-            this.ws ={};
-        }else if((typeof window === "object") && window.WebSocket){
-            this.ws = new WebSocket(this.url);   
-        }else if(typeof WebSocket === "undefined"){
-            this.ws =  new  (require("ws"))(this.url); 
+            this.ws = {};
+        } else if ((typeof window === "object") && window.WebSocket) {
+            this.ws = new WebSocket(this.url);
+        } else if (typeof WebSocket === "undefined") {
+            this.ws = new (require("ws"))(this.url);
         }
- 
+
         this.ws.onclose = (e) => {
-            if(! this._close_by_self )
-              this._start();
+            if (!this._close_by_self)
+                this._start();
         }
         this.ws.onopen = () => {
- 
-            let login = ()=>{
-                this.send({ do: "login", id: this.epii_id, unique:this.epii_info["unique"]?1:0,info: this.epii_info,cb:this._pushcb((data)=>{
-                    if(data.code-1==0){
-                         this.is_ready = true;
-                         this.ready_callbacks.forEach(cb => cb());
-                         //增加心跳，否则总是自动断线
-                         this._heart_rate();
-                    }else{
-                        if(this.__on_error) this.__on_error({msg:"it is has exist username "+this.epii_id+" "}) ;  
-                    }
-                   
-                }) },true);
+
+            let login = () => {
+                this.send({
+                    do: "login", id: this.epii_id, unique: this.epii_info["unique"] ? 1 : 0, info: this.epii_info, cb: this._pushcb((data) => {
+                        if (data.code - 1 == 0) {
+                            this.is_ready = true;
+                            this.ready_callbacks.forEach(cb => cb());
+                            //增加心跳，否则总是自动断线
+                            this._heart_rate();
+                        } else {
+                            if (this.__on_error) this.__on_error({ msg: "it is has exist username " + this.epii_id + " " });
+                        }
+
+                    })
+                }, true);
             }
-            if(this.epii_info.hasOwnProperty("__unique__") && ( this.epii_info.__unique__===1 ) ){
-                this.ping(this.epii_id,(ret)=>{
-                   if(ret.code-1==0)
-                   {
-                      if(this.__on_error) this.__on_error({msg:"exist id "+this.epii_id})      ; 
-                   }else{
-                    login();
-                   }
-                })   
-           }else{
-               login();
-           }
- 
+            if (this.epii_info.hasOwnProperty("__unique__") && (this.epii_info.__unique__ === 1)) {
+                this.ping(this.epii_id, (ret) => {
+                    if (ret.code - 1 == 0) {
+                        if (this.__on_error) this.__on_error({ msg: "exist id " + this.epii_id });
+                    } else {
+                        login();
+                    }
+                })
+            } else {
+                login();
+            }
+
         };
         this.ws.onmessage = (e) => {
             try {
@@ -94,40 +113,39 @@ class epii_websocket {
             }
 
         }
-        this.ws.onerror = (e)=> {
-              if(! this._close_by_self )
-                setTimeout(()=>{
+        this.ws.onerror = (e) => {
+            if (!this._close_by_self)
+                setTimeout(() => {
                     this._start();
-                },2000)
+                }, 2000)
 
-              if(this.__on_error) this.__on_error(e)      ;
+            if (this.__on_error) this.__on_error(e);
         }
         if (typeof uni != "undefined") {
-              uni.onSocketOpen(this.ws.onopen);
-              uni.onSocketError(this.ws.onerror);
-              uni.onSocketMessage(this.ws.onmessage);
-              uni.onSocketClose(this.ws.onclose); 
-              this.ws.send =function(data){
-                uni.sendSocketMessage({data:data})
-              } 
-              this.ws.close =function(){
+            uni.onSocketOpen(this.ws.onopen);
+            uni.onSocketError(this.ws.onerror);
+            uni.onSocketMessage(this.ws.onmessage);
+            uni.onSocketClose(this.ws.onclose);
+            this.ws.send = function (data) {
+                uni.sendSocketMessage({ data: data })
+            }
+            this.ws.close = function () {
                 uni.closeSocket();
-              } 
-              uni.connectSocket({
+            }
+            uni.connectSocket({
                 url: this.url
-              });
+            });
         }
     }
 
-    onUserAvailable(userid,cb){
-        if(!this.onUserAvailable_cbs[userid])
-        {
+    onUserAvailable(userid, cb) {
+        if (!this.onUserAvailable_cbs[userid]) {
             this.onUserAvailable_cbs[userid] = [];
         }
         this.onUserAvailable_cbs[userid].push(cb);
-        this.send({ do: "onUserAvailable", toid: userid ,id:this.epii_id});
+        this.send({ do: "onUserAvailable", toid: userid, id: this.epii_id });
     }
-    
+
     regServer(name, handler) {
         this.epii_servers[name] = handler;
         this.send({ do: "regServer", name: name });
@@ -136,25 +154,25 @@ class epii_websocket {
         this.send({ do: "callServer", id: id, name: name, data: data, cb: this._pushcb(cb) });
     }
     sendTo(id, name, data, cb) {
-        this.send({ do: "callServer", id: id, name: name,more:1, data: data, cb: this._pushcb(cb) });
+        this.send({ do: "callServer", id: id, name: name, more: 1, data: data, cb: this._pushcb(cb) });
     }
-    ping(id,cb){
-        this.callServer(id,"__ping",{__ping:1}, cb)
+    ping(id, cb) {
+        this.callServer(id, "__ping", { __ping: 1 }, cb)
     }
-    exit(){
+    exit() {
         this._close_by_self = true;
         this.ws.close();
     }
-    close(){
+    close() {
         this.exit();
     }
     __callServer(data) {
         if (this.epii_servers.hasOwnProperty(data.name)) {
-                this.epii_servers[data.name]({ data: data.data, client: data.client }, (ret) => {
-                    if (data.cb - 1 != -2) {
-                        this.send({ do: "reponseCall", connect: data.connect, data: ret, cb: data.cb });
-                    }
-                })
+            this.epii_servers[data.name]({ data: data.data, client: data.client }, (ret) => {
+                if (data.cb - 1 != -2) {
+                    this.send({ do: "reponseCall", connect: data.connect, data: ret, cb: data.cb });
+                }
+            })
         }
     }
     __callback(data) {
@@ -162,35 +180,37 @@ class epii_websocket {
             this.cbs[data.cb](data.data);
         }
     }
-    __onUserAvailable(data){
-        if(this.onUserAvailable_cbs[data.id])
-        {
+    __onUserAvailable(data) {
+        if (this.onUserAvailable_cbs[data.id]) {
             delete data.do;
-             this.onUserAvailable_cbs[data.id].forEach(cb=>cb(data));
+            this.onUserAvailable_cbs[data.id].forEach(cb => cb(data));
         }
 
     }
 
-    send(data,noReady) {
-        if(noReady){
+    send(data, noReady) {
+        if (noReady) {
             this.ws.send(JSON.stringify(data))
-        }else 
-        this.ready(()=>{
-            this.ws.send(JSON.stringify(data))
-        });
-        
+        } else
+            this.ready(() => {
+                this.ws.send(JSON.stringify(data))
+            });
+
     }
     isReady() {
         return this.is_ready;
     }
     ready(cb) {
+        const [_cb, ret] = wrapPromise(cb);
         if (this.is_ready) {
-            cb();
+            _cb();
         } else {
-            this.ready_callbacks.push(cb);
+            this.ready_callbacks.push(_cb);
         }
+        if (ret)
+            return ret;
     }
-    onError(cb){
+    onError(cb) {
         this.__on_error = cb;
     }
 }
